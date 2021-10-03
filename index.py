c5b6ae065fb833fb24bc88163e7a92a8abd524ce
zryoung <riyang@qq.com>
2021-10-03 22:27:42 +0800

    download by threading

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from requests import *
 from json import dumps, loads
 from os import mkdir, chdir
+from queue import Queue
+import threading
 
 num = post("https://api.loj.ac/api/problem/queryProblemSet", headers={
     "Content-Type": "application/json"
@@ -34,8 +36,8 @@ def downloadProblem(id):
         mkdir(str(id));
     except Exception as e:
         pass
-    chdir(str(id));
-    with open("Description.md", "w+", encoding="utf-8") as f:
+    # chdir(str(id));
+    with open(str(id) + "/Description.md", "w+", encoding="utf-8") as f:
         # f.write()
         content = dat["localizedContentsOfLocale"]["contentSections"]
         for i in content:
@@ -47,7 +49,7 @@ def downloadProblem(id):
                 f.write("### Output\n```\n" + dat["samples"][i["sampleId"]]["outputData"] + "```\n");
 
     #get tag name
-    with open("problem.yaml", "w+") as f:
+    with open(str(id) + "/problem.yaml", "w+") as f:
         f.write("owner:2\n")
         f.write("title:" + dat["localizedContentsOfLocale"]["title"] + "\n")
         f.write("tags:\n")
@@ -57,12 +59,12 @@ def downloadProblem(id):
             f.write(" - " + i["name"] + "\n")
 
     try:
-        mkdir("testData");
+        mkdir(str(id) + "/testData");
     except Exception as e:
         pass
     # chdir("testData")
     #get "config" file
-    with open("testData/config.yaml", "w+") as f:
+    with open(str(id) + "/testData/config.yaml", "w+") as f:
         judgeInfo = dat["judgeInfo"]
         if "timeLimit" in judgeInfo.keys():
             f.write("time:" + str(judgeInfo["timeLimit"]) + "ms\n")
@@ -81,27 +83,54 @@ def downloadProblem(id):
     for i in URList:
         resp = get(i["downloadUrl"])
         try:
-            with open("testData/" + i["filename"], "w+") as f:
+            with open(str(id) + "/testData/" + i["filename"], "w+") as f:
                 f.write(resp.text);
         except Exception as e:
-            with open("testData/" + i["filename"], "wb+") as f:
+            with open(str(id) + "/testData/" + i["filename"], "wb+") as f:
                 f.write(resp.content);
-    chdir("..")
+    # chdir("..")
     print("No." + str(id) + " Done");
 
 
+class worker(threading.Thread):
+    def __init__(self, q):
+        threading.Thread.__init__(self)
+        self.queue = q
+
+    def run(self):
+        while True:
+            if self.queue.empty():
+                break
+            id = queue.get()
+
+            try:
+                # print("thread %s is running..." %threading.current_thread().name)
+                # print("%d downloading"%id)
+                downloadProblem(id)
+            except Exception as e:
+                print(e)
+            self.queue.task_done()
 
 nowi = 0;
+queue = Queue(maxsize=10)
+# num=25
 try:
-    for i in range(14, num, 8):
+    for i in range(0, num, 8):
         list = post("https://api.loj.ac/api/problem/queryProblemSet", headers={"Content-Type": "application/json"},
                     data=dumps({"locale": "zh_CN", "skipCount": i, "takeCount": 8})).json()["result"]
         for j in list:
             nowi = j["meta"]["displayId"];
-            downloadProblem(j["meta"]["displayId"])
+            # downloadProblem(j["meta"]["displayId"])
+            queue.put(nowi)
+
+            if threading.activeCount() < 10:
+                t = worker(queue)
+                t.start()
 
 except KeyboardInterrupt as e:
     print("Download Interupted...\n Saving Files... ", end="");
     with open("history.dat", "w+") as f:
         f.write(str(nowi))
     print("Done");
+
+queue.join()
