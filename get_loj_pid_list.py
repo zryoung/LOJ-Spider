6c6eb11ce3dcb141b0c00d917015a5824faa275d
zryoung <riyang@qq.com>
2024-08-25 03:54:31 +0800

    修复单独下载pid_list

--- get_loj_pid_list.py
+++ get_loj_pid_list.py
@@ -1,3 +1,4 @@
+import json
 import os
 from config import DOWNLOAD_PATH
 from get_by_schedule import get_pid_list
@@ -6,4 +7,4 @@ from get_by_schedule import get_pid_list
 pid_list = get_pid_list()
 
 with open(os.path.join(DOWNLOAD_PATH,'pid_list.txt'), 'w') as f:
-    f.write(pid_list)
+    json.dump(pid_list, f, indent=4)
