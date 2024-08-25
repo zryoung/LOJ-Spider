698a6a55ddbb561b56484e6673c9bb7052147ca9
zryoung <riyang@qq.com>
2024-08-25 21:12:53 +0800

    @logger.catch

--- util.py
+++ util.py
@@ -1,11 +1,10 @@
-
-
 import requests
 from loguru import logger
 from tenacity import retry, stop_after_attempt, wait_random
 
-# @logger.catch
-@retry(stop=stop_after_attempt(5),wait=wait_random(1, 3),reraise=True)
+
+@logger.catch
+@retry(stop=stop_after_attempt(5),wait=wait_random(1, 3), reraise=True)
 def request_post(url, headers=None, data=None, json=None, stream=True,verify=False, timeout=(5, 5)):
     return requests.post(
         url,
@@ -17,7 +16,7 @@ def request_post(url, headers=None, data=None, json=None, stream=True,verify=Fal
         json=json,
     )
 
-# @logger.catch
+@logger.catch
 @retry(stop=stop_after_attempt(5),wait=wait_random(1, 3),reraise=True)
 def request_get(url, params=None, headers=None, stream=False,verify=False, timeout=(5, 5)):
     return requests.get(
