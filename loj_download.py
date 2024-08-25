aae3cd205177928bf5d7c268bb3eb08fc24a8491
zryoung <riyang@qq.com>
2024-08-25 12:46:28 +0800

    关闭异常输出，因为tenacity最后一次失败会捕获异常

--- loj_download.py
+++ loj_download.py
@@ -35,62 +35,62 @@ LanguageMap = {
 
 @retry(stop=stop_after_attempt(5), wait=wait_random(1, 3), reraise=True)
 def resume_download(url, file_path, retry=3):
-    try:
-        # 第一次请求是为了得到文件总大小
-        r1 = request_get(url, stream=True, verify=False)
-        # 有些文件没有conteng-length这个信息
-        if not r1.headers.get("Content-Length"):
-            # print('hi')
-            with open(file_path, 'ab') as file:
-                file.write(r1.content)
+    # try:
+    # 第一次请求是为了得到文件总大小
+    r1 = request_get(url, stream=True, verify=False)
+    # 有些文件没有conteng-length这个信息
+    if not r1.headers.get("Content-Length"):
+        # print('hi')
+        with open(file_path, 'ab') as file:
+            file.write(r1.content)
 
-            # print(f'{file_path}下载完成')
-            return
-        
-        total_size = int(r1.headers["Content-Length"])
-
-        # 这重要了，先看看本地文件下载了多少
-        if os.path.exists(file_path):
-            temp_size = os.path.getsize(file_path)  # 本地已经下载的文件大小
-        else:
-            os.makedirs(os.path.dirname(file_path), exist_ok=True)
-            temp_size = 0
-        # 显示一下下载了多少
-        # print(f'已下载:{temp_size}')
-        # print(f'文件总大小:{total_size}')
-
-        if temp_size >= total_size:
-            return
-        # 核心部分，这个是请求下载时，从本地文件已经下载过的后面下载
-        headers = {"Range": f"bytes={temp_size}-"}
-        res = request_get(url, stream=True, headers=headers,timeout=(6.1,21.1))
-
-        with open(file_path, "ab") as file:
-            for chunk in res.iter_content(chunk_size=1024):
-                if chunk:
-                    temp_size += len(chunk)
-                    file.write(chunk)
-                    file.flush()
-
-                    ###这是下载实现进度显示####
-                    done = int(50 * temp_size / total_size)
-                    sys.stdout.write(
-                        f"\r[{'█' * done}{' ' * (50 - done)}] {int(100 * temp_size / total_size):3d}% \t{file_path} "
-                    )
-                    sys.stdout.flush()
-        print()  # 避免上面\r 回车符
         # print(f'{file_path}下载完成')
-    except Exception as e:
-        # data={
-        #     "time": time.localtime(),
-        #     "message":str(e),
-        #     "file": file_path,
-        #     "download_url":url
-        # }
-        # file_writer('fail.json', json.dumps(data, ensure_ascii=False))
-        logger.error(f'{file_path}出错重试.{e}')
-        raise Exception(f'{file_path}出错重试.{e}')
-        # print(f'Error:"message:"{e},"file:"{file_path},"url:"{url}')
+        return
+    
+    total_size = int(r1.headers["Content-Length"])
+
+    # 这重要了，先看看本地文件下载了多少
+    if os.path.exists(file_path):
+        temp_size = os.path.getsize(file_path)  # 本地已经下载的文件大小
+    else:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        temp_size = 0
+    # 显示一下下载了多少
+    # print(f'已下载:{temp_size}')
+    # print(f'文件总大小:{total_size}')
+
+    if temp_size >= total_size:
+        return
+    # 核心部分，这个是请求下载时，从本地文件已经下载过的后面下载
+    headers = {"Range": f"bytes={temp_size}-"}
+    res = request_get(url, stream=True, headers=headers,timeout=(6.1,21.1))
+
+    with open(file_path, "ab") as file:
+        for chunk in res.iter_content(chunk_size=1024):
+            if chunk:
+                temp_size += len(chunk)
+                file.write(chunk)
+                file.flush()
+
+                ###这是下载实现进度显示####
+                done = int(50 * temp_size / total_size)
+                sys.stdout.write(
+                    f"\r[{'█' * done}{' ' * (50 - done)}] {int(100 * temp_size / total_size):3d}% \t{file_path} "
+                )
+                sys.stdout.flush()
+    print()  # 避免上面\r 回车符
+    # print(f'{file_path}下载完成')
+    # except Exception as e:
+    #     # data={
+    #     #     "time": time.localtime(),
+    #     #     "message":str(e),
+    #     #     "file": file_path,
+    #     #     "download_url":url
+    #     # }
+    #     # file_writer('fail.json', json.dumps(data, ensure_ascii=False))
+    #     logger.error(f'{file_path}出错重试.{e}')
+    #     raise Exception(f'{file_path}出错重试.{e}')
+    #     # print(f'Error:"message:"{e},"file:"{file_path},"url:"{url}')
 
 def file_writer(filename, content):
     with open(os.path.join(__dirname,filename), 'a', encoding='utf-8') as file:
@@ -363,7 +363,7 @@ def get_problem(protocol, host, pid):
             threads.append(thread)            
         except Exception as e:
             logger.error(f'{pid} 数据下载出错。错误原因：{e}')
-            raise Exception(f'{pid} 数据下载出错。错误原因：{e}')
+            # raise Exception(f'{pid} 数据下载出错。错误原因：{e}')
     for thread in threads:
         thread.join()
 
