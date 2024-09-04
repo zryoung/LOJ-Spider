56db7191c05c7041b62692b5ee7b142415e4f7a8
zryoung <riyang@qq.com>
2024-09-04 19:42:47 +0800

    从https://cemc.uwaterloo.ca下载数据

--- get_cemc_uwaterloo.py
+++ get_cemc_uwaterloo.py
@@ -1,23 +1,54 @@
 
 
-from util import request_get
+import os
 
+from loguru import logger
+from util import request_get, resume_download
+from lxml import etree
+from bs4 import BeautifulSoup as bs
+from requests import packages
+from config import DOWNLOAD_PATH
 
-host = 'https://cemc.uwaterloo.ca/'
-url = host + 'ajax'
-params = {
-    "grade":20,
-    "academic_year":"All",
-    "contest_category":29,
-    "view_name":"listing",
-    "view_display_id":"past_contest",
-}
-headers={
-    "Cookie": "SSESSd51f0751c2e4bacf872f152c09e2bbb0=HSTlAxiqrSfJC%2CO%2CIY6sb8DKZWMqptTcaU7qGwcc4DK44DC0",
-    "host": host,
-    "Content-Type": "application/json",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.18 Safari/537.36 Edg/93.0.961.10",
-}
+packages.urllib3.disable_warnings()  # 去除警告信息
 
-result = request_get(url,params=params,headers=headers)
-print(result)
+if __name__ == '__main__':
+    hosts = 'https://cemc.uwaterloo.ca/'
+    prefix = hosts + 'resources/past-contests'
+    # print(prefix)
+    host = 'uwaterloo'
+    problem_path = os.path.join(DOWNLOAD_PATH, host)
+    os.makedirs(problem_path, exist_ok=True)
+
+    
+    for page in range(4):
+        url = f'https://cemc.uwaterloo.ca/resources/past-contests?grade=All&academic_year=All&contest_category=29&page={page}'
+        result = request_get(
+            url,
+            # params=params
+        )
+        html = result.content
+        soup = bs(html, "html.parser")
+        table = soup.find("tbody")
+        rows = table.find_all("tr")
+        for row in rows:
+            try:
+                data = row.find_all("td")
+                title = data[0].getText().strip()
+                year = data[1].getText().strip()
+                contest = data[3].find_all("a", attrs={"download":"download"})[0].get('href')
+                solution = data[4].find_all("a", attrs={"download":"download"})[0].get('href')
+                new_path = os.path.join(problem_path, title, year)
+                os.makedirs(new_path, exist_ok=True)
+                # print(new_path + contest.split('/')[-1])
+                resume_download(hosts + contest, os.path.join(new_path, contest.split('/')[-1]))
+                resume_download(hosts + solution, os.path.join(new_path, solution.split('/')[-1]))
+                print(title, year, contest, solution)
+            except Exception as e:
+                logger.error(e)
+        
+        try:
+            # 下一页
+            url = prefix + soup.find("a", attrs={"class":"page-link align-items-center", "rel":"next"}).get('href')
+        except Exception as e:
+            url = ''
+        print(url)
