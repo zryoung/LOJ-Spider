3f9346f976c4a198a9a59d30d6f3e3156fb3417f
zryoung <zryoung>
2025-04-27 10:25:51 +0800

    refactor(config): 更新下载路径并添加USER_AGENTS列表
    更新了DOWNLOAD_PATH以指向新的测试目录，并添加了USER_AGENTS列表以支持多种浏览器用户代理字符串，提高请求的多样性和兼容性。

--- config.py
+++ config.py
@@ -1,5 +1,18 @@
-DOWNLOAD_PATH = '../downloads'
+DOWNLOAD_PATH = '../downloads/test'
 LUOGU_COOKIE = '__client_id=f6dcf4dc35af786067357111855214487e71a7ca; _uid=6476; C3VK=6be832'
 # LUOGU_COOKIE = '_uid=6476; __client_id=a5ef8858a8ece67466ee3719b21cb5914b5b11f0'
 
 DOWNLOAD_PATH_USACO = r'd:/usaco1'
+USER_AGENTS = [
+    # 创建10个不同的User-Agent字符串
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",  # Firefox
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59",  # Edge
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 OPR/77.0.4054.203",  # Opera
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Vivaldi/4.2",  # Vivaldi
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Brave/91.0.4472.124",  # Brave
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Yandex/21.8.1.807",  # Yandex
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 QQBrowser/11.0.5639.400",  # QQ Browser
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 UC Browser/12.1.0.1024",  # UC Browser
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 360SE",  # 360 Browser (360安全浏览器)    
+]
